35b33cec47901b534e4301f0fafb263f0dc62c73
Abhishek Yadav <abhishekaug2002@gmail.com>
2022-11-15 19:57:00 +0530

    code updated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import doclab from "../../Assets/Projects/Doclab.jpg";
+import medinvoice from "../../Assets/Projects/MEDINVOICE.jpg";
+import recruit from "../../Assets/Projects/recruit.jpg";
+import levitation from "../../Assets/Projects/levitation.jpg";
+import space from "../../Assets/Projects/space.jpg";
+import share from "../../Assets/Projects/share.jpg";
 
 function Projects() {
   return (
@@ -23,7 +23,7 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={recruit}
               isBlog={false}
               title="Recruitment"
               description="A recruitment single page static website in ReactJs with lots of creative animations and almost responsive elements."
@@ -34,7 +34,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={share}
               isBlog={false}
               title="One Time Share"
               description="My personal developed project to upload the files which would be saved in a database and generate a link to download the file and after using that file the link becomes invalid."
@@ -44,7 +44,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={medinvoice}
               isBlog={false}
               title="MEDINVOICE"
               description="Project made in a hackathon organized by DS Community. It is a platform which solves the problem of keeping old prescriptions. It starts to store all your health records at single place."
@@ -54,7 +54,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={doclab}
               isBlog={false}
               title="DOCLAB"
               description="Project made in a hackathon organized by Hackoheist. We developed a platform where all students can upload there projects and showcase there work to all the world and we have developed the plgarism checker to have the organilatiy of uploaded files."
@@ -64,7 +64,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={levitation}
               isBlog={false}
               title="LEVITATION"
               description="Project made in a hackathon organized by IIT BOMBAY. It provides a platform where user can book there travelling or waiting tickets for hyperloops pods and it have such a developed time management that a user never have to wait more than 5 minutes."
@@ -74,7 +74,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={space}
               isBlog={false}
               title="Space Erudite Game"
               description="Project made in NASA SPACE CHALLENGE 2021. It is a basic text based javascript game developed over analyzing various space situations and environment categories of the planets in our solar system."
