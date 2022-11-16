ea44e8c5580def4561effc7bf9e7c719ab5aaaef
Abhishek Yadav <abhishekaug2002@gmail.com>
2022-11-16 22:46:48 +0530

    Code updated

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -4,15 +4,13 @@ import { CgCPlusPlus } from "react-icons/cg";
 import {
   DiJavascript1,
   DiReact,
-  DiNodejs,
   DiMongodb,
   DiPython,
   DiGit,
 } from "react-icons/di";
 import {
-  SiPytorch,
-  SiFirebase,
-  SiNextdotjs,
+  SiHtml5,
+  SiCss3,
   SiFlask
 } from "react-icons/si";
 
@@ -23,26 +21,23 @@ function Techstack() {
         <CgCPlusPlus />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+        <SiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiCss3 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col> */}
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
       </Col>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
-      </Col> */}
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
       </Col>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col> */}
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
       </Col>
