da1e5ac4bd82c4137aec54c6106f749d6873bd2a
Abhishek Yadav <abhishekaug2002@gmail.com>
2023-10-12 22:30:37 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -83,7 +83,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://drive.google.com/file/d/1Sh98-1L9G4nFH-ga0x0vZI1jP3B1yXB5/view?usp=sharing"
+                href="https://drive.google.com/file/d/1zNQFnnD8Rt1qVM1SEulnK5FXJ1MFQT0c/view?usp=sharing"
                 target="_blank"
                 className=""
               >
