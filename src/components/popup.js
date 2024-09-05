93004f68c22ed1f9681fa985fef799e1e1241e7f
Abhishek Yadav <abhishekaug2002@gmail.com>
2024-09-05 18:56:16 +0530

    Updated resume

--- src/components/popup.js
+++ src/components/popup.js
@@ -5,8 +5,8 @@ function Popup({ onClose }) {
     <div className="popup-overlay">
       <div className="popup-content">
         <button className="popup-close" onClick={onClose}>X</button>
-        <a href="https://drive.google.com/file/d/1ynfETIHvl3SC3HWTiH2bPgvm_xJtPQmA/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="popup-button">SDE RESUME</a>
-        <a href="https://drive.google.com/file/d/1P-L2P35lom_mSpEJMdmTcc1Ioa7mIoBk/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="popup-button">FULLSTACK RESUME</a>
+        <a href="https://drive.google.com/file/d/1cjUGKCRz4QLPp06Ku9JK1YMj3rAyp5C8/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="popup-button">SDE RESUME</a>
+        <a href="https://drive.google.com/file/d/143xqmvPnWHz5mWNfzMj2uk60SRO4Fi9W/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="popup-button">FULLSTACK RESUME</a>
         <a href="https://drive.google.com/file/d/1MI99sbEcOqhx-e327nIWNAdmgAIx1Ytb/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="popup-button">IOS RESUME</a>
       </div>
     </div>
